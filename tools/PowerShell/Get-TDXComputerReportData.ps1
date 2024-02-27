72792bad91fccac96a699c9b5d4b5b65d0b39992
Joe Schlimmer <joseph.schlimmer@gmail.com>
2024-02-27 13:23:56 -0500

    powershell tool to get a json object for a tdx report

---
+++ tools/PowerShell/Get-TDXComputerReportData.ps1
@@ -0,0 +1,24 @@
+
+
+$endpoint = "https://help.ithaca.edu/TDWebApi/api/reports/1966?withData=true"
+# Grab a token string from https://help.ithaca.edu/TDWebApi/api/auth/loginsso
+# Replace YOUR_BEARER_TOKEN with the token string
+$token = "YOUR_BEARER_TOKEN"
+
+$headers = @{
+    "Authorization" = "Bearer $token"
+    "Content-Type" = "application/json"
+}
+
+#TODO: FUNCTION: Test bearer token expiration and refresh if needed
+
+# Get a REST response
+$response = Invoke-WebRequest -Uri $endpoint -Headers $headers -Method Get
+
+# Output the response content in pretty Json format
+$jsonObject = $response | ConvertFrom-Json
+$jsonObject
+
+# Select the SerialNumber property and output
+$serialNumbers = $jsonObject.DataRows | Select-Object -ExpandProperty SerialNumber
+$serialNumbers
